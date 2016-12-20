d533ffd151e94a1d0ea1d6c184069489c5acb262
Flave <david@flave.co.uk>
2016-12-20 20:07:48 +0000

    Added basic Steam API Communication that will fetch user and achievement information.

---
+++ Mica.Core/Communication/Steam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using RestSharp;
+
+namespace Mica.Communication
+{
+    public class Steam
+    {
+        private static string GetAuthKey()
+        {
+            var authFileContents = File.ReadAllLines((AppDomain.CurrentDomain.BaseDirectory + @"Auth\AuthKey.txt"));
+            return string.Join("", authFileContents);
+        }
+
+        private IRestResponse Connect(string resource)
+        {
+            var client = new RestClient("http://api.steampowered.com");
+            var request = new RestRequest(resource, Method.GET);
+
+            return client.Execute(request);
+        }
+
+        public string GetAchievementsForGame(int appId, string userId)
+        {
+            appId = 211420;
+            userId = "76561198040630790";
+            var authKey = GetAuthKey();
+
+            var userStats = Connect($"ISteamUserStats/GetUserStatsForGame/v0002/?appid={appId}&key={authKey}&steamid={userId}");
+            var gameInfo = Connect($"ISteamUserStats/GetSchemaForGame/v2/?appid={appId}&key={authKey}");
+            
+            return userStats.Content;
+        }
+    }
+}
--- Mica.WebApp/Controllers/HomeController.cs
+++ Mica.WebApp/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Mica.Communication;
 
 namespace WebApplication1.Controllers
 {
@@ -10,6 +7,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            var test = new Steam().GetAchievementsForGame(0, "");
             return View();
         }
 
